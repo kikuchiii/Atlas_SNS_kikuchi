929e0900e64ff9e27ecf72cead704c8042e85f0f
kikuchiii <junkkk0753@yahoo.co.jp>
2022-09-13 17:16:38 +0900

    seederで初期ユーザーを登録しました。

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //以下を追加します
+        DB::table('users')->insert([
+'username' => '菊池',
+'mail' => 'kikuchi@yahoo.co.jp',
+'password' => bcrypt('kikuchi') ,//後ほど調べる
+        ]);
+    }
+}
